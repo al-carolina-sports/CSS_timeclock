b03b8e157c51712757755dd2538883fb8242f896
Al Antony <alvin.antony@carolinasportsandspine.com>
2026-09-19 23:06:02 -0400

    feat: add css-timeclock-addon/css-timeclock-addon.php

---
+++ css-timeclock-addon/css-timeclock-addon.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Name:       CSS Time Clock Addon
+ * Plugin URI:        https://wordpress.org/plugins/aio-time-clock-lite/
+ * Description:       PIN pad and name-list kiosk add-on for All in One Time Clock Lite. Shared tablets clock employees in and out without a WordPress login. Writes AIO-compatible shift records so Real Time Monitoring still works.
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.4
+ * Author:            CSS
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       css-timeclock-addon
+ *
+ * @package CssTimeclockAddon
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'CSS_TC_ADDON_VERSION', '1.0.0' );
+define( 'CSS_TC_ADDON_FILE', __FILE__ );
+define( 'CSS_TC_ADDON_DIR', plugin_dir_path( __FILE__ ) );
+define( 'CSS_TC_ADDON_URL', plugin_dir_url( __FILE__ ) );
+define( 'CSS_TC_ADDON_BASENAME', plugin_basename( __FILE__ ) );
+
+require_once CSS_TC_ADDON_DIR . 'includes/class-plugin.php';
+
+/**
+ * Plugin bootstrap.
+ *
+ * @return Css_Tc_Plugin
+ */
+function css_tc_addon() {
+	return Css_Tc_Plugin::instance();
+}
+
+css_tc_addon();
+
+register_activation_hook( CSS_TC_ADDON_FILE, array( 'Css_Tc_Plugin', 'activate' ) );
+register_deactivation_hook( CSS_TC_ADDON_FILE, array( 'Css_Tc_Plugin', 'deactivate' ) );
